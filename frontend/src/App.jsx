55c8c540a0deb50af361379c246cba78f89ad7ce
Andaloussi212 <zayd.benz.pro@proton.me>
2026-06-24 11:22:34 +0200

    Création et modification de la page d'accueil React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>ZDrive</h1>
+      <p>Frontend React initialisé avec Vite</p>
+      <p>Projet créée et développée par 4ndalussi</p>
+    </>
+  );
+}
+
+export default App;
