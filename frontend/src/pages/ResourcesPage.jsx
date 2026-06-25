46128f7d5aaf70756c29531de03e634fc1e55810
Andaloussi212 <zayd.benz.pro@proton.me>
2026-06-25 15:26:20 +0200

    titre adaptatif

--- frontend/src/pages/ResourcesPage.jsx
+++ frontend/src/pages/ResourcesPage.jsx
@@ -1,10 +1,13 @@
-import { resources } from '../data/mockData';
+import { resources, subjects } from '../data/mockData';
 import ResourceCard from '../components/ResourceCard';
 import { useParams } from 'react-router-dom';
 
 function ResourcesPage() {
   const { subjectId } = useParams();
   const selectedSubjectId = Number(subjectId);
+  const selectedSubject = subjects.find(
+    (subject) => subject.id === selectedSubjectId
+  );
   const filteredResources = resources.filter(
     (resource) => resource.subjectId === selectedSubjectId
   );
@@ -12,7 +15,9 @@ function ResourcesPage() {
     <>
       <main className="resources-page">
         <header className="page-header">
-          <h1>Ressources</h1>
+          <h1>
+            {selectedSubject ? selectedSubject.name : 'Matière Introuvable'}
+          </h1>
           <p>Liste des fichiers disponibless pour cette matière</p>
         </header>
 
--- frontend/src/pages/SubjectsPage.jsx
+++ frontend/src/pages/SubjectsPage.jsx
@@ -1,10 +1,13 @@
-import { subjects } from '../data/mockData';
+import { subjects, semesters } from '../data/mockData';
 import SubjectCard from '../components/SubjectCard';
 import { useParams } from 'react-router-dom';
 
 function SubjectsPage() {
   const { semesterId } = useParams();
   const selectedSemesterId = Number(semesterId);
+  const selectedSemester = semesters.find(
+    (semester) => semester.id === selectedSemesterId
+  );
   const filteredSubjects = subjects.filter(
     (subject) => subject.semesterId === selectedSemesterId
   );
@@ -12,7 +15,9 @@ function SubjectsPage() {
     <>
       <main className="subjects-page">
         <header className="page-header">
-          <h1>Matières</h1>
+          <h1>
+            {selectedSemester ? selectedSemester.name : 'Semestre introuvable'}
+          </h1>
           <p>Liste des matières disponibles pour ce semestre</p>
         </header>
 
